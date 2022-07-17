e95ecdc22950b5839a3b054740f6c9c460b9694f
hayasitakahiro <0kv1td3.7254p9e@au.com>
2022-07-17 15:16:56 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,14 @@ class ItemsController < ApplicationController
 def index
 end
 
+
+
+
+
+private
+
+def message_params
+  params.require(:items).permit(:content, :image).merge(user_id: current_user.id)
+end
+
 end
